bf238b15022ef13474613ff76838d5f9bcbddca3
snamiki1212 <snamiki1212@gmail.com>
2021-05-24 18:51:28 -0700

    Update restaurant cell

--- ExampleCollection/Controllers/RestaurantCollectionViewCell.swift
+++ ExampleCollection/Controllers/RestaurantCollectionViewCell.swift
@@ -9,28 +9,45 @@ import UIKit
 
 class RestaurantCollectionViewCell: UICollectionViewCell {
     static let cellId = "CELL_ID"
-    var label = UILabel();
     var restaurant: Restaurant? {
         didSet {
             updateUI()
         }
     }
     
+    private var stack = UIStackView();
+    private var nameLabel = UILabel()
+    private var typeLabel = UILabel()
+    private var priceLabel = UILabel()
+    
     private func updateUI(){
-        label.text = restaurant?.name
+        guard let restaurant = restaurant else { return }
+        nameLabel.text = restaurant.name
+        typeLabel.text = restaurant.type
+        priceLabel.text = restaurant.price
     }
 
     override init(frame: CGRect) {
         super.init(frame: frame)
-        contentView.layoutIfNeeded()
-        contentView.addSubview(label)
-        label.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(stack)
+        
+        // auto-layout
+        stack.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            label.topAnchor.constraint(equalTo: contentView.topAnchor),
-            label.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
-            label.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
-            label.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            stack.topAnchor.constraint(equalTo: contentView.topAnchor),
+            stack.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            stack.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            stack.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
         ])
+        
+        // contents
+        stack.addArrangedSubview(nameLabel)
+        stack.addArrangedSubview(typeLabel)
+        stack.addArrangedSubview(priceLabel)
+        
+        // stlyes
+        stack.axis = .vertical
+        stack.alignment = .center
     }
     
     required init?(coder: NSCoder) {
